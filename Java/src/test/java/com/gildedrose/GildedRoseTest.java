edfa328d9deacf63ebe7cfaf6000e461ba6a1e9e
Jeremy Cook <jeremycook0@icloud.com>
2019-11-02 09:26:07 -0400

    Moved to use approvals testing library.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,20 +1,13 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 public class GildedRoseTest {
 
@@ -28,11 +21,13 @@ public class GildedRoseTest {
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6)};
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)};
+        // this conjured item does not work properly yet
+        // Add it when you can!
+        //new Item("Conjured Mana Cake", 3, 6)};
         List<String> results = new ArrayList<>();
         GildedRose app = new GildedRose(items);
+
         IntStream.rangeClosed(1, 20)
                 .forEach(day -> {
                     Arrays.stream(items)
@@ -42,16 +37,6 @@ public class GildedRoseTest {
                 });
         String result = String.join("\n", results);
 
-        assertEquals(getExpectedResult(), result);
-    }
-
-    private String getExpectedResult() {
-        try {
-            URL resource = getClass().getResource("/GoldMaster.txt");
-            return Files.readString(Paths.get(resource.toURI()));
-        } catch (IOException | URISyntaxException e) {
-            fail("Unable to read resource file");
-            return "";
-        }
+        Approvals.verify(result);
     }
 }
