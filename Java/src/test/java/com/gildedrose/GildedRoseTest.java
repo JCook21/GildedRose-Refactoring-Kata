f427b1defece7017a59b704ddb2be8eba28bb7d6
Jeremy Cook <jeremycook0@icloud.com>
2020-01-15 14:10:43 -0500

    Cosmetic change.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -21,10 +21,11 @@ public class GildedRoseTest {
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)};
-        // this conjured item does not work properly yet
-        // Add it when you can!
-        //new Item("Conjured Mana Cake", 3, 6)};
+                // this conjured item does not work properly yet
+                // Add it when you can!
+                //new Item("Conjured Mana Cake", 3, 6),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
+        };
         List<String> results = new ArrayList<>();
         var app = new GildedRose(items);
 
